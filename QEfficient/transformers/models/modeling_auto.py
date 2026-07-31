8b1fd3a4bc1ad9f97701d5c7ad9cdff6a146ec37
Kushal Dulla <kdulla@qti.qualcomm.com>
2026-07-31 17:30:02 +0530

    formatting and remove debug prints
    Signed-off-by: Kushal Dulla <kdulla@qti.qualcomm.com>

--- QEfficient/transformers/models/modeling_auto.py
+++ QEfficient/transformers/models/modeling_auto.py
@@ -1701,8 +1701,8 @@ class _QEffAutoModelForImageTextToTextDualQPC:
             and getattr(self.lang_model.hash_params["blocking_kwargs"], "batch_fold", False)
         )
 
-        export_seq_len = min(prefill_seq_len, constants.ONNX_EXPORT_EXAMPLE_SEQ_LEN)
-        
+        export_seq_len = min(prefill_seq_len, constants.ONNX_EXPORT_EXAMPLE_SEQ_LEN) if prefill_seq_len else None
+
         # TODO This is a temporary change as continous batching is enabled only for few models. Once support is added for all the models this exception handing can be removed.
         try:
             inputs = self.model.get_dummy_inputs(
